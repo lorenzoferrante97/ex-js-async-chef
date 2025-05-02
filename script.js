baf5209794228653d5a2e44973fcf85f1a5f5e37
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-05-02 17:10:38 +0200

    add: try catch

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 const getChef = async (id) => {
   const resRecipe = await fetch(`https://dummyjson.com/recipes/${id}`);
   const recipe = await resRecipe.json();
-  const resChef = await fetch(`https://dummyjson.com/users/${recipe.userId}`);
+  const resChef = await fetch(`aaahttps://dummyjson.com/users/${recipe.userId}`);
   const chef = await resChef.json();
 
   return chef;
@@ -13,13 +13,14 @@ const getChef = async (id) => {
 
 const getChefBirthday = async (id) => {
   const chef = await getChef(id);
-
-  console.log('chef: ', chef);
-
   return chef.birthDate;
 };
 
 (async () => {
-  const chefBirthday = await getChefBirthday(3);
-  console.log('Data di nascita dello chef: ', chefBirthday);
+  try {
+    const chefBirthday = await getChefBirthday(3);
+    console.log('Data di nascita dello chef: ', chefBirthday);
+  } catch (err) {
+    console.error(err.message);
+  }
 })();
