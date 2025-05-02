91a1bab6cf2345471aeb84cd0f6f38aa4e33e287
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-05-02 16:26:15 +0200

    add: get recipe

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 // start code
+
+const getRecipe = async (id) => {
+  const resRecipe = await fetch(`https://dummyjson.com/recipes/${id}`);
+  const recipeJson = await resRecipe.json();
+
+  return recipeJson;
+};
+
+const getChefBirthday = async (id) => {
+  const recipe = getRecipe(id);
+
+  return recipe;
+};
+
+(async () => {
+  const recipe = getChefBirthday(3);
+  console.log('ricetta: ', recipe);
+})();
